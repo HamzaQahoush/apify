77eca44cb6af08643e6a383452bf3a5c0586d5e1
Hamza Qahoush <hamza.qah@gmail.com>
2022-06-26 20:20:24 +0300

    add permissions

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,37 +1,39 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, authentication
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from .permissions import IsStaffEditorPermission
+
 
 class ProductDetailview(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
-    def perform_create(self,serializer):
-        title=serializer.validated_data.get('title')
-        content=serializer.validated_data.get('content') or None
-        if not content :
-            content=title
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if not content:
+            content = title
         serializer.save(content=content)
-        
-    
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
+    authentication_classes = [authentication.SessionAuthentication]
+
 
 class ProductUpdateView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field= 'pk'
+    lookup_field = "pk"
 
     def perform_update(self, serializer):
         # we can do some action while updating , send signal
-        # instance=serializer.save() 
+        # instance=serializer.save()
         # if not instance.content:
         #   instance.content= instance.title
         return super().perform_update(serializer)
@@ -40,16 +42,16 @@ class ProductUpdateView(generics.UpdateAPIView):
 class ProductDeleteView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field= 'pk'
+    lookup_field = "pk"
 
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)
 
 
-# function view : 
-@api_view(['GET', 'POST'])
+# function view :
+@api_view(["GET", "POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
-    method = request.method 
+    method = request.method
 
     if method == "GET":
         if pk is not None:
@@ -58,7 +60,7 @@ def product_alt_view(request, pk=None, *args, **kwargs):
             data = ProductSerializer(obj, many=False).data
             return Response(data)
         # list view
-        queryset = Product.objects.all() 
+        queryset = Product.objects.all()
         data = ProductSerializer(queryset, many=True).data
         return Response(data)
 
@@ -66,8 +68,8 @@ def product_alt_view(request, pk=None, *args, **kwargs):
         # create an item
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content') or None
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
             if content is None:
                 content = title
             serializer.save(content=content)
